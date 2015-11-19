4c9b1d500338e261d5086690032c78fc1db1a982
Roger Panella <rogerpanella@gmail.com>
2015-11-19 12:41:59 -0600

    finished Sinatra pair exercise

--- app.rb
+++ app.rb
@@ -1,2 +1,32 @@
 require 'bundler'
 Bundler.require
+
+get '/' do
+  our_team = {
+    :number_of_members => 2,
+    :sex_of_members => 'male',
+    :color_shirts => 'navy',
+    :do_we_rock => true
+  }
+  return our_team.to_json
+end
+
+get '/dan_tabion' do
+  dan = {
+    :age => 39,
+    :speed => 'slow',
+    :favorite_pianist => 'too many',
+    :todays_lunch =>'good shit'
+  }
+  return dan.to_json
+end
+
+get '/roger_panella' do
+  roger = {
+    :favorite_food => 'dope vegan food',
+    :favorite_guitarist => 'Jim Hall',
+    :dogs_name => 'Sasha',
+    :number_of_feet => 2
+  }
+  return roger.to_json
+end
